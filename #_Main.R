0d38fa147f21ec8c5d6afd07eb5c2c985595fa7d
Charlene_MSI_AI <q2330416@gmail.com>
2025-06-20 15:10:08 +0800

    Ori

---
+++ #_Main.R
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+# Merge multiple samples
+seurat_all <- merge(seurat_2901, y = list(seurat_3080),
+                    add.cell.ids = c("HC2901","HC3080"))
+
+seurat_list <- SplitObject(seurat_all, split.by = "orig.ident")
+
+
+# cell cycle
+s.genes <- cc.genes$s.genes
+g2m.genes <- cc.genes$g2m.genes
+
+# Normalize and score cell cycle for each sample
+seurat_list <- lapply(X = seurat_list, FUN = function(x) {
+  x <- NormalizeData(x,normalization.method = "LogNormalize", scale.factor = 10000)
+  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
+  x <- ScaleData(x, features = rownames(x), verbose = TRUE)
+  x <- CellCycleScoring(x, s.features = s.genes, g2m.features = g2m.genes, set.ident = FALSE)
+})
+
+
+# Data integration
+features <- SelectIntegrationFeatures(seurat_list)
+anchors <- FindIntegrationAnchors(seurat_list, dims = 1:30)
+integrated <- IntegrateData(anchors, dims = 1:30)
+
+# Dimensionality reduction and clustering
+DefaultAssay(integrated) <- "integrated"
+integrated <- ScaleData(integrated, vars.to.regress = c("S.Score", "G2M.Score", "percent.mt"))
+integrated <- RunPCA(integrated, npcs = 50) %>% RunUMAP(dims = 1:30)
+integrated <- FindNeighbors(integrated, dims = 1:30) %>% FindClusters(resolution = 0.3)
