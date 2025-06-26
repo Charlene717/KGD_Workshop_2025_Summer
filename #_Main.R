1cf30cb96d189e028fd6edd38b5062113659c064
Charlene_MSI_AI <q2330416@gmail.com>
2025-06-26 15:48:12 +0800

    source("RUN_Integration.R")

--- #_Main.R
+++ #_Main.R
@@ -37,40 +37,9 @@ source("Sample_GSM6111845.R")
 source("Sample_GSM6111847.R")
 
 
-#### Merge multiple samples ####
-seurat_all <- merge(seurat_GSM6111844, y = list(seurat_GSM6111845,seurat_GSM6111847),
-                    add.cell.ids = c("GSM6111844","GSM6111845","GSM6111847"))
 
-seurat_all <- JoinLayers(seurat_all)  # 🔥 在 merge 後統一執行 JoinLayers
-
-seurat_list <- SplitObject(seurat_all, split.by = "orig.ident")
-
-
-# cell cycle
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.genes
-
-# Normalize and score cell cycle for each sample
-seurat_list <- lapply(X = seurat_list, FUN = function(x) {
-  x <- NormalizeData(x,normalization.method = "LogNormalize", scale.factor = 10000)
-  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
-  x <- ScaleData(x, features = rownames(x), verbose = TRUE)
-  x <- CellCycleScoring(x, s.features = s.genes, g2m.features = g2m.genes, set.ident = FALSE)
-})
-
-
-# Data integration
-features <- SelectIntegrationFeatures(seurat_list)
-anchors <- FindIntegrationAnchors(seurat_list, dims = 1:30)
-integrated <- IntegrateData(anchors, dims = 1:30)
-
-# Dimensionality reduction and clustering
-DefaultAssay(integrated) <- "integrated"
-integrated <- ScaleData(integrated, vars.to.regress = c("S.Score", "G2M.Score", "percent.mt"))
-integrated <- RunPCA(integrated, npcs = 50) %>% RunUMAP(dims = 1:30)
-integrated <- FindNeighbors(integrated, dims = 1:30) %>% FindClusters(resolution = 0.3)
-
-DimPlot(integrated, reduction = "umap")
+#### Data Integration ####
+source("RUN_Integration.R")
 
 
 #### Cell Type Annotation ####
